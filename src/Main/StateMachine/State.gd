3a82bdaea5d68e4b407051ebc4c2193ab5bfd256
The Honey Badger <32531175+Keyaki777@users.noreply.github.com>
2021-03-12 15:10:19 -0300

    Add state machine

---
+++ src/Main/StateMachine/State.gd
@@ -0,0 +1,33 @@
+extends Node
+
+class_name State, "res://assets/icons/state.svg" 
+
+"""
+State Interface to use in Hierarchical State Machines.
+The lowest leaf tries to handle callbacks, and if it can´t, it delegates the work to its parent.
+It´s up to the user to call the parent state´s functions, e.g.
+'get_parent()'.physics_process(delta)'
+"""
+
+onready var _state_machine: = _get_state_machine(self)
+
+func _ready() -> void:
+	set_physics_process(false)
+	set_process_unhandled_input(false)
+
+func unhandled_input(event: InputEvent) -> void:
+	return
+
+func physics_process(delta: float) -> void:
+	return
+
+func enter (msg: Dictionary = {}) -> void:
+	return
+
+func exit() -> void:
+	return
+	
+func _get_state_machine( node: Node) -> Node:
+	if node != null and not node.is_in_group("state_machine"):
+		return _get_state_machine(node.get_parent())
+	return node
---
+++ src/Main/StateMachine/StateMachine.gd
@@ -0,0 +1,44 @@
+extends Node
+
+class_name StateMachine, "res://assets/icons/state_machine.svg"
+
+"""
+Generic State Machine. Initializes states and delegate3s engine callbacks 
+(_physics_process, unhandled_input) to the active state.
+"""
+export var initial_state: = NodePath() 
+
+onready var state: State = get_node(initial_state) setget set_state
+onready var _state_name: = state.name
+
+
+func _init() -> void:
+	add_to_group("state_machine")
+	return
+ 
+
+func _ready() -> void:
+	yield(owner, "ready")
+	state.enter()
+	
+	
+func _unhandled_input(event: InputEvent) -> void:
+	state.unhandled_input(event)
+
+
+func _physics_process(delta: float) -> void:
+	state.set_physics_process(delta)
+	
+func transition_to(target_state_path: String, msg: Dictionary ={}) -> void:
+	if not has_node(target_state_path):
+		return
+	var target_state: = get_node(target_state_path)
+	
+	
+	state.exit()
+	self.state = target_state
+	state.enter(msg)
+	
+func set_state(value: State) -> void:
+	state = value
+	_state_name = value.name
